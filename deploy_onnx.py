38cbf384dcfc74289a90a93eed0fda7045e2f65d
chloetang28 <chloeatang@utexas.edu>
2024-03-20 17:28:16 -0500

    Updated deploy_onnx

--- deploy_onnx.py
+++ deploy_onnx.py
@@ -5,40 +5,63 @@ import os
 from PIL import Image
 import argparse
 import time
+import torch
+from torch.utils.data import Dataset, DataLoader
+from zipfile import ZipFile
+from io import BytesIO
 
-# TODO: create argument parser object
-parser = argparse.ArgumentParser(description='ECE361E HW3 - Deployment code')
-# TODO: add one argument for selecting VGG or MobileNet-v1 models
-parser.add_argument('--model', type=str, help='Model that will be used')
-parser.add_argument('--device', type=str,help='Deployment device' )
-# TODO: Modify the rest of the code to use those arguments correspondingly
+class NYU_Testset(Dataset):
+    def __init__(self, zip_path):
+        input_zip=ZipFile(zip_path)
+        data = {name: input_zip.read(name) for name in input_zip.namelist()}
+        
+        self.rgb = np.load(BytesIO(data['eigen_test_rgb.npy'])).type(torch.float32) #Range [0,1]
+        self.depth = np.load(BytesIO(data['eigen_test_depth.npy'])).type(torch.float32) #Range[0, 10]
+
+    def __getitem__(self, idx):
+        image = self.rgb[idx]
+        depth = self.depth[idx]
+        return image, depth
+
+    def __len__(self):
+        return len(self.rgb)
+    
+
+parser = argparse.ArgumentParser(description='ONNX Deployment code')    
+
+parser.add_argument('--model', type=str,help='Model to be deployed' )
 args = parser.parse_args()
-device_name = args.device
+
 model_name = args.model
-onnx_model_name = f"{device_name}_{model_name}.onnx"  # TODO: insert ONNX model name, essentially the path to the onnx model
+onnx_model_name = f"{model_name}.onnx" 
+
 # Create Inference session using ONNX runtime
 sess_options = rt.SessionOptions()
 sess = rt.InferenceSession(onnx_model_name)
 sess_options.intra_op_num_threads=4
 sess = rt.InferenceSession(onnx_model_name,sess_options)
+
 # Get the input name for the ONNX model
 input_name = sess.get_inputs()[0].name
 print("Input name  :", input_name)
+
 # Get the shape of the input
 input_shape = sess.get_inputs()[0].shape
 print("Input shape :", input_shape)
+
 # Mean and standard deviation
 mean = np.array((0.4914, 0.4822, 0.4465))
 std = np.array((0.2023, 0.1994, 0.2010))
-# Label names for CIFAR10 Dataset
-label_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-test_correct = 0
-test_total = 0
-start_time = time.time()
+
+# start_time = time.time()
+
+dataset = NYU_Testset("/home/orin/Documents/FH12_23-24/EdgeMapperv2/hw/nyu.zip")
+
+
 # The test_deployment folder contains all 10.000 images from the testing dataset of CIFAR10 in .png format
-for filename in tqdm(os.listdir("/home/student/HW3_files/test_deployment")):
+for image in dataset.depth:
     # Take each image, one by one, and make inference
-    with Image.open(os.path.join("/home/student/HW3_files/test_deployment", filename)).resize((32, 32)) as img:
+    with Image.open(image).resize((32, 32)) as img:
         # print("Image shape:", np.float32(img).shape)
         # normalize image
         input_image = (np.float32(img) / 255. - mean) / std
@@ -49,17 +72,7 @@ for filename in tqdm(os.listdir("/home/student/HW3_files/test_deployment")):
         # print("Input Image shape:", input_image.shape)
         # Run inference and get the prediction for the input image
         pred_onnx = sess.run(None, {input_name: input_image})[0]
-        
-        # Find the prediction with the highest probability
-        top_prediction = np.argmax(pred_onnx[0])
-        # Get the label of the predicted class
-        pred_class = label_names[top_prediction]
-                
-        # TODO: compute test accuracy of the model
-        gt = filename.replace('.png', '').split('_')[1]
-                
-        test_total += 1
-        test_correct += 1 if pred_class == gt else 0
-end_time = time.time()
-print('Test accuracy: %.2f %%' % (100. * test_correct / test_total))
-print('Total inference time:', (end_time - start_time))
+        # Remove Batch axis
+        pred_image = np.squeeze(pred_onnx)
+
+# end_time = time.time()
